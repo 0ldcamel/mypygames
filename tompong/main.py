b838de5f0f6528090611b041e3ca01567da8fd0f
0ld Camel <0ldcamel502@gmail.com>
2024-07-21 16:51:26 +0300

    added tompong/main

--- tompong/main.py
+++ tompong/main.py
@@ -1,1 +1,10 @@
 print('Hello')
+try:
+    import sys, random, math, os
+    import getopt, pygame
+    from socket import *
+    from pygame.locals import *
+
+except ImportError as err:
+    print(f'could not load module. {err}')
+    sys.exit(2)
