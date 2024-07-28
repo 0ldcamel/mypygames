0a02ca05f4be94abd9400f9730d78534029cb84d
0ld Camel <0ldcamel502@gmail.com>
2024-07-28 16:24:56 +0300

    asdf

--- alsweigart/cat/catanimation.py
+++ alsweigart/cat/catanimation.py
@@ -1,21 +1,29 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 pygame.init()
 
 FPS = 30
+DISPLAY_W = 800
+DISPLAY_H = 600
 fpsClock =pygame.time.Clock()
 
-DISPLAYSURF = pygame.display.set_mode((800, 600), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((DISPLAY_W, DISPLAY_H), 0, 32)
 pygame.display.set_caption('0ld Camel')
 
 catImg = pygame.image.load('cat.png')
-mouseImg = pygame.image.load('mouse.jpg')
-mousex = 720
-mousey = 520
+mouseImg_r = pygame.image.load('mouse.jpg')
+mouseImg_l = pygame.image.load('mouse_l.png')
+mouseRect = mouseImg_l.get_rect()
+# mousex = DISPLAY_W - mouseRect[2] - 10
+# mousey = DISPLAY_H - mouseRect[3] - 10
+mousex = random.randint(10, DISPLAY_W - 10)
+mousey = random.randint(10, DISPLAY_H - 10)
 catx = 10
 caty = 10
 direction = 'right'
+
+mouse_dir = [random.randint(0, 5), random.randint(0, 5)]
 
 while True:
     DISPLAYSURF.fill('white')
@@ -36,6 +44,23 @@ while True:
         caty -= 5
         if caty == 10:
             direction = 'right'
+    
+    mousex += mouse_dir[0]
+    mousey += mouse_dir[1]
+    mouseImg = mouseImg_r
+    
+    if mousex < 10:
+        mouse_dir[0] = random.randint(3, 7)
+        mouseImg = mouseImg_r
+    elif mousex + mouseRect.right > DISPLAY_W - 10:
+        mouse_dir[0] = -random.randint(3, 7)
+        mouseImg = mouseImg_r
+
+    elif mousey < 10:
+        mouse_dir[1] = random.randint(3, 7)
+    elif mousey + mouseRect.bottom > DISPLAY_H - 10:
+        mouse_dir[1] = -random.randint(3, 7)
+
     DISPLAYSURF.blit(catImg, (catx, caty))
     DISPLAYSURF.blit(mouseImg, (mousex, mousey))
 
